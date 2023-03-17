604a2ed93b488a5a928ca4ef3809829b701426ea
Manu S Rao <67941652+MANUSRAO@users.noreply.github.com>
2023-03-17 13:40:49 +0530

    Update urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.home),
+    path('',views.home,name="Home"),
     path('heatmap/',views.heatmap,name="Nifty"),
     path('sensexHeatmap/',views.sensexHeatmap,name="Sensex")
 ]
